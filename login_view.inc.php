9595f68770713139807c3526494c72cc04896ce9
developer-austine <152804218+developer-austine@users.noreply.github.com>
2024-09-28 07:36:44 -0700

    Add files via upload

---
+++ login_view.inc.php
@@ -0,0 +1,20 @@
+<?php 
+
+declare(strict_types = 1);
+
+function check_login_errors() {
+    if(isset($_SESSION["errors_login"])) {
+        $errors = $_SESSION["errors_login"];
+
+        echo "<br>";
+
+        foreach ($errors as $error) {
+            echo '<p class="form-error">' . $error . '</p>';
+        }
+
+        unset($_SESSION["errors_login"]);
+    } else if(isset($_GET['login']) && $_GET['login'] === "success") {
+        echo '<br>';
+        echo '<p class="form-success">Login success!</p>';
+    }
+}
